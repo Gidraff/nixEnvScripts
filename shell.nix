d3ae0c44e7ccbb62e8fee4fe6a6cff1b37353f8e
Gidraff <kamandegid@gmail.com>
2024-03-04 20:50:33 +0300

    feat: creates a nix shell environment - add a nix file - add a bash script [Done#]

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+    packages = with pkgs; [
+        lolcat
+        cowsay
+    ];
+
+    GREETING = "Hello, Nix!";
+
+    shellHook = ''
+        echo $GREETING | cowsay | lolcat
+    '';
+}
